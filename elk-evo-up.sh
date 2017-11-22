b29f1ddc9d5e19eb107128395cdad70c1ed758b2
Stefan Prodan <stefan.prodan@gmail.com>
2017-11-22 14:34:56 +0200

    evo setup

--- elk-evo-up.sh
+++ elk-evo-up.sh
@@ -3,21 +3,24 @@ set -e
 
 cd elk
 
-ESDATA1_HOST=swarm-worker-1 \
-ESDATA1_IP=10.156.0.5 \
-ESDATA2_HOST=swarm-worker-2 \
-ESDATA2_IP=10.156.0.3 \
-ESDATA3_HOST=swarm-worker-3 \
-ESDATA3_IP=10.156.0.4 \
-ESROUTER_HOST=swarm-manager-1 \
-ESROUTER_IP=10.156.0.2 \
-ESDATA_RAM=2G \
-ESDATA_RAM_LIMIT=4G \
-ESROUTER_RAM=512M \
-ESROUTER_RAM_LIMIT=1G \
-docker stack deploy -c docker-compose.yml elk
+export DOCKER_HOST=$1
 
-export ES_PUBLIC_IP=35.198.178.32
+ESDATA1_HOST=cluster03 \
+ESDATA1_IP=172.16.200.118 \
+ESDATA2_HOST=cluster06 \
+ESDATA2_IP=172.16.200.117 \
+ESDATA3_HOST=cluster02 \
+ESDATA3_IP=172.16.200.114 \
+ESROUTER_HOST=cluster04 \
+ESROUTER_IP=172.16.200.115 \
+ESDATA_RAM=16G \
+ESDATA_RAM_LIMIT=32G \
+ESROUTER_RAM=6G \
+ESROUTER_RAM_LIMIT=12G \
+ESDATA_PATH=/mnt/disk1/nglm/nglm-base-runtime \
+docker stack deploy -c evo-compose.yml elk
+
+export ES_PUBLIC_IP=172.16.200.115
 
 echo "Waiting for ES Cluster to be online and healthy..."
 until $(curl -o /dev/null -s -XGET "${ES_PUBLIC_IP}:9200/_cluster/health?wait_for_status=green&timeout=50s&pretty"); do
