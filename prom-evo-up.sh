6a99833729a4651b6b18ebaaf8c599db162a04e4
Stefan Prodan <stefan.prodan@gmail.com>
2017-11-24 02:20:15 +0200

    set data path

--- prom-evo-up.sh
+++ prom-evo-up.sh
@@ -7,6 +7,7 @@ export DOCKER_HOST=$1
 
 PROM_HOST=cluster01 \
 PROM_IP=172.16.200.84 \
+PROM_PATH=/mnt/disk1/nglm/nglm-base-runtime/prom \
 docker stack deploy -c docker-compose.yml prom
 
 docker service ls | grep prom_
