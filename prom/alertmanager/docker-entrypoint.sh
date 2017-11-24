5590532599ebdffaf45348b52849c4d8d8d26e44
Stefan Prodan <stefan.prodan@gmail.com>
2017-11-24 02:46:32 +0200

    prom port

--- prom/alertmanager/docker-entrypoint.sh
+++ prom/alertmanager/docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -e
 
-USER root
-
 cat /etc/alertmanager/alertmanager.yml |\
     sed "s@#api_url: <url>#@api_url: '$SLACK_URL'@g" |\
     sed "s@#channel: <channel>#@channel: '#$SLACK_CHANNEL'@g" |\
