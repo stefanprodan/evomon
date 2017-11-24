4faea1723a03f36b43b57809aa7ec7f71d6dbb3c
Stefan Prodan <stefan.prodan@gmail.com>
2017-11-24 11:16:16 +0200

    check if stack exists

--- prom-evo-up.sh
+++ prom-evo-up.sh
@@ -5,6 +5,12 @@ cd prom
 
 export DOCKER_HOST=$1
 
+if [ -n "$(docker stack ls | grep prom)" ]; then
+    echo ""
+else
+    docker stack rm prom
+fi
+
 PROM_HOST=cluster01 \
 PROM_IP=172.16.200.84 \
 PROM_PATH=/mnt/disk1/nglm/nglm-base-runtime/prom \
